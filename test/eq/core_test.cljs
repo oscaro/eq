2294a947450dba4bfafdeafcf5ee423fc920e317
Baptiste Fontaine <b@ptistefontaine.fr>
2017-07-03 22:18:22 +0200

    Add selection tests

--- test/eq/core_test.cljs
+++ test/eq/core_test.cljs
@@ -1,7 +1,16 @@
 (ns eq.core-test
   (:require [eq.core :as eq]
-            [cljs.test :refer-macros [deftest is testing]]
+            [cljs.test :refer-macros [deftest is are testing]]
             [cljs.nodejs :as node]))
 
 (deftest build-selector
-  (is (nil? (eq/build-selector "foo"))))
+  (is (nil? (eq/->selector "foo"))))
+
+(deftest select-empty-selector
+  (let [sel (eq/->selector "")]
+    (is (some? sel))
+    (are [data] (= [data] (eq/select sel [data]))
+       {}
+       {:a 1 :b 2}
+       {"" 42}
+       )))
